c490ecdf191b8152ac1abafec43ae416277d46e6
Felicity <felicitybongo@gmail.com>
2020-07-30 17:19:51 -0400

    adding extra files from vue web extension

---
+++ src/popup/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id='app'>
+        <HeaderBar/>
+        <div class="main-section columns">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+<script>
+import HeaderBar from '@/components/header-bar.vue';
+
+export default {
+  name: 'App',
+  components: { HeaderBar }
+}
+</script>
+
+
+<style lang="scss">
+@import '../assets/css/styles.scss';
+html {
+  width: 400px;
+  height: 400px;
+}
+</style>
+</style>
