78bccae5235b2daf5ffd40602051e4da426d3379
Felicity Bongo <felicity.bongo@waystar.com>
2020-07-29 21:20:28 -0400

    fixing bug in home view

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
+        <HeaderBar/>
         <NavBar></NavBar>
     </div>
 </template>
 
 <script>
 import NavBar from '@/components/nav-bar.vue';
+import HeaderBar from '@/components/header-bar.vue';
+
     export default {
         name: "home",
         components:{
             NavBar,
+            HeaderBar,
         },
     }
 </script>
