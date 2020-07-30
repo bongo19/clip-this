27e91851eeacb672be8f4e2d0921181aec3f5751
Felicity Bongo <felicity.bongo@waystar.com>
2020-07-30 13:32:58 -0400

    styling changes. Currently done with Dropbox Oauth2

--- src/views/dropbox.vue
+++ src/views/dropbox.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="content-container">
-        <h1>WELCOME TO CLIP IT!!!</h1>
-        <p>Your token is - {{token}}</p>
+        <header class="card-header flex-container">
+                <p class="card-header-title">Currently Connected to Dropbox</p>
+        </header>
     </div>
 </template>
 
 <script>;
+import Axios from 'axios';
     export default {
         name:'Dropbox',
         data() {
@@ -17,7 +19,21 @@
             getAccessToken() {
                 const tokenHash = location.hash.substring(13);
                 this.token = tokenHash.substring(0,tokenHash.indexOf("&"));
-                console.log(this.token.substring(13,this.token.indexOf("&")));
+                //console.log(this.token.substring(13,this.token.indexOf("&")));
+            },
+            async getFilesInClipFolder(){
+                const result = await Axios({
+                    url: 'https://api.dropboxapi.com/2/file_requests/list_v2',
+                    data:{
+                        'limit':1000,
+                    },
+                    headers:{
+                        'Authorization': `Bearer ${this.token}`,
+                        'Content-Type':'application/json',
+                    }
+                });
+
+                return result.data;
             }
         },
         mounted(){
