f4ffe3526522e445939faa6d11b6e18137ee26be
Felicity Bongo <felicity.bongo@waystar.com>
2020-07-29 21:14:59 -0400

    adding home view (for real this time) and refactoring code to fit home view

--- src/helpers/router.js
+++ src/helpers/router.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Home from '@/views/Home.vue';
+import Dropbox from '@/views/Dropbox.vue';
 
 Vue.use(Router);
 
@@ -8,6 +10,7 @@ export default new Router({
     mode: 'history',
     routes: [
         { path: '/', redirect: '/home' },
-        { path: '/path', component: component },
+        { path: '/home', name: 'home', component: Home },
+        { path: '/dropbox', name: 'dropbox', component: Dropbox },
     ]
 });
---
+++ src/views/home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <NavBar></NavBar>
+    </div>
+</template>
+
+<script>
+import NavBar from '@/components/nav-bar.vue';
+    export default {
+        name: "home",
+        components:{
+            NavBar,
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
